f89761e7dcce56a83f0749b4f82c4706e0725ca7
a1vaa <106130590+a1vaa@users.noreply.github.com>
2025-03-28 20:51:24 -0300

    Add files via upload

---
+++ TAD_SET.h
@@ -0,0 +1,18 @@
+#ifndef TAD_SET_H
+#define TAD_SET_H
+#include "TAD_string.h"
+typedef struct nodo{
+	str dato;
+	struct nodo * sig;
+}T_Nodo;
+typedef T_Nodo * set;
+set createSet();
+void appenSet(set*,str);
+void showSet(set);
+set strToSetToken(str.char);
+set unionSet(set,set);
+set intersetionSEt(set,set);
+set deferenciaSet(set,set);
+int pertenence(str,set);
+int include(set,set);
+#endif
